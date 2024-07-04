2e2df741fb600e59d141ed74308fc5b1887b9a3a
salman-alvi2k <102091911+salman-alvi2k@users.noreply.github.com>
2024-07-04 16:12:30 +0500

    question no. 12 filter element from the array

---
+++ Filter_Element_From_Array.js
@@ -0,0 +1,9 @@
+var filter = function (arr, fn) {
+  let returnedArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      returnedArray.push(arr[i]);
+    }
+  }
+  return returnedArray;
+};
